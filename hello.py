1a205a4b3915eec3292d7878d476a8dc85ff0da8
NikitaPetaichuk <31537936+NikitaPetaichuk@users.noreply.github.com>
2020-06-07 17:51:00 +0300

    Delete hello.py

--- hello.py
+++
@@ -1,4 +0,0 @@
-def wsgi_application(environ, start_response):
-    response_data = bytes("\r\n".join(environ["QUERY_STRING"].split("&")), encoding="uft8")
-    start_response("200 OK", [("Content-Type", "text/plain")])
-    return iter([response_data])
